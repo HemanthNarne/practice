0d27f5ea2108b7069af1d2f42e66434d659a4af9
Modali,Naga Sravanthi <s528749@ch112017m.nwmissouri.edu>
2018-02-16 16:30:59 -0600

    added code to populate tableview in the first page

--- Smart-Attendance/ViewController.swift
+++ Smart-Attendance/ViewController.swift
@@ -8,8 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate  {
+    
+    let role:[String] = ["Faculty", "Student"]
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return role.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "role_cell")!
+        cell.textLabel?.text = role[indexPath.row]
+        return cell
+    }
+    
+    @IBOutlet weak var roleTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
